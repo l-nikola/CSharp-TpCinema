7db87f7e8f56710137360f567c1cf68c2965a3fc
Nikola Legay <147250203+l-nikola@users.noreply.github.com>
2023-11-29 18:25:17 +0100

    CSharp-TpCinema

---
+++ Conception d'une classe/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conception_d_une_classe
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string reduction, seance;
+            int nombrePlaceDispo, nbre, nb;
+            double taux, affaire, salle, salle1, salle2;
+            bool tarifReduit, SeanceFini;
+            SalleCinema s1 = new SalleCinema("Sacré Graal", 60, 7.5);
+            s1.Afficher();
+            nombrePlaceDispo = s1.nbPlacesDisponibles();
+            Console.WriteLine("Combien de places souhaitez vous acheter ?");
+            nbre = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Possédez vous une réduction ? (o/n)");
+            reduction = Console.ReadLine();
+            if (reduction == "o")
+            {
+                tarifReduit = true;
+            }
+            else
+            {
+                tarifReduit = false;
+            }
+            s1.vendrePlaces(nbre, tarifReduit);
+            taux = s1.tauxRemplissage();
+            Console.WriteLine("La salle est donc remplie à " + taux + "%");
+            affaire = s1.chiffreAffaires();
+            Console.WriteLine("Le cinéma a donc fait un chiffre d'affaire de " + affaire + " euros");
+            Console.WriteLine("--------------------------------");
+
+            SalleCinema s2 = new SalleCinema("Matrix", 40, 6.5);
+            s2.Afficher();
+            nombrePlaceDispo = s2.nbPlacesDisponibles();
+            Console.WriteLine("Combien de places souhaitez vous acheter ?");
+            nbre = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Possédez vous une réduction ? (o/n)");
+            reduction = Console.ReadLine();
+            if (reduction == "o")
+            {
+                tarifReduit = true;
+            }
+            else
+            {
+                tarifReduit = false;
+            }
+            s2.vendrePlaces(nbre, tarifReduit);
+            taux = s2.tauxRemplissage();
+            Console.WriteLine("La salle est donc remplie à " + taux + "%");
+            affaire = s2.chiffreAffaires();
+            Console.WriteLine("Le cinéma a donc fait un chiffre d'affaire de " + affaire + " euros");
+            Console.WriteLine("--------------------------------");
+
+            salle = s1.compare();
+            salle1 = salle;
+            salle = s2.compare();
+            salle2 = salle;
+
+            if (salle1 == salle2)
+            {
+                Console.WriteLine("0");
+                Console.WriteLine("--------------------------------");
+            }
+            if (salle1 > salle2)
+            {
+                Console.WriteLine("1");
+                Console.WriteLine("--------------------------------");
+            }
+            if (salle1 < salle2)
+            {
+                Console.WriteLine("-1");
+                Console.WriteLine("--------------------------------");
+            }
+            
+            nb = s2.getSalle() + s1.getSalle() + 2 ;
+            Console.WriteLine("Il y à " + nb + " salles");
+            Console.WriteLine("--------------------------------");
+
+            Console.WriteLine("La séance de la salle 1 est-elle terminée ? (o/n)");
+            seance = Console.ReadLine();
+            if (seance == "o")
+            {
+                SeanceFini = true;
+            }
+            else
+            {
+                SeanceFini = false;
+            }
+            s1.remiseAZero(SeanceFini);
+
+            Console.WriteLine("La séance de la salle 2 est-elle terminée ? (o/n)");
+            seance = Console.ReadLine();
+            if (seance == "o")
+            {
+                SeanceFini = true;
+            }
+            else
+            {
+                SeanceFini = false;
+            }
+            s2.remiseAZero(SeanceFini);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Conception d'une classe/SalleCinéma.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conception_d_une_classe
+{
+    class SalleCinema
+    {
+        string _film;
+        int _nombreSiege, _venduTarifNormal, _venduTarifReduit, _nbsalle;
+        double _tarifNormal;
+
+        public SalleCinema(string film, int nombreSiege, double tarifNormal)
+        {
+            _film = film;
+            _nombreSiege = nombreSiege;
+            _venduTarifNormal = 0;
+            _venduTarifReduit = 0;
+            _tarifNormal = tarifNormal;
+            _nbsalle = 0;
+        }
+        public int getSalle()
+        {
+            return _nbsalle;
+        }
+        public string getFilm()
+        {
+            return _film;
+        }
+        public int getNombreSiege()
+        {
+            return _nombreSiege;
+        }
+
+        public int getVenduTarifNormal()
+        {
+            return _venduTarifNormal;
+        }
+
+        public int getVenduTarifReduit()
+        {
+            return _venduTarifReduit;
+        }
+        public double getTarifNormal()
+        {
+            return _tarifNormal;
+        }
+
+        public void setTarifNormal(double tarifNormal)
+        {
+            _tarifNormal = tarifNormal;
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine("Film joué : " + _film);
+            Console.WriteLine("Nombre de places : " + _nombreSiege);
+            Console.WriteLine("Prix d'une place : " + _tarifNormal);
+            Console.WriteLine(_venduTarifNormal + " places vendues au tarif normal");
+            Console.WriteLine(_venduTarifReduit + " places vendues au tarif réduit");
+            Console.WriteLine("--------------------------------");
+        }
+
+        public int nbPlacesDisponibles()
+        {
+            int nombrePlaceDispo;
+            nombrePlaceDispo = _nombreSiege - (_venduTarifNormal + _venduTarifReduit);
+            Console.WriteLine("Il y a " + nombrePlaceDispo + " places disponibles");
+            Console.WriteLine("--------------------------------");
+            return nombrePlaceDispo;
+        }
+
+        public void vendrePlaces(int nbre, bool tarifReduit)
+        {
+            double PrixReduction, Prix;
+
+            if (nbre > _nombreSiege)
+            {
+                Console.WriteLine("Nombre de place disponible insuffisante, la vente n'est pas possible");
+            }
+            else
+            {
+                if (tarifReduit == true)
+                {
+                    PrixReduction = _tarifNormal * 0.8;
+                    PrixReduction = PrixReduction * nbre;
+                    _venduTarifReduit = nbre;
+                    Console.WriteLine("Le prix de vos places sont donc de : " + PrixReduction + " euros");
+                    Console.WriteLine("--------------------------------");
+                }
+                else
+                {
+                    Prix = _tarifNormal * nbre;
+                    _venduTarifNormal = nbre;
+                    Console.WriteLine("Le prix de vos places sont donc de : " + Prix + " euros");
+                    Console.WriteLine("--------------------------------");
+                }
+            }
+        }
+
+        public void remiseAZero(bool SeanceFini)
+        {
+            if (SeanceFini == true)
+            {
+                _venduTarifNormal = 0;
+                _venduTarifReduit = 0;
+                Console.WriteLine("Les compteurs ont été remis à 0");
+                Console.WriteLine("--------------------------------");
+            }
+            else
+            {
+                Console.WriteLine("Veillez attendre la fin de la séance");
+                Console.WriteLine("--------------------------------");
+            }
+        }
+
+        public double chiffreAffaires()
+        {
+            double affaire;
+
+            affaire = (_venduTarifNormal * _tarifNormal) + _venduTarifReduit * (0.8 * _tarifNormal);
+
+            return affaire;
+        }
+
+        public double tauxRemplissage()
+        {
+            double taux;
+
+            taux = 100 * (_venduTarifReduit + _venduTarifNormal) / _nombreSiege;
+            return taux;
+        }
+        public double compare()
+        {
+            double salle, affaire;
+
+            salle = affaire = tauxRemplissage();
+            return salle;
+        }
+        public static int Salle()
+        {
+            int nb;
+            nb = 0;
+            nb = nb + 1;
+            return nb;
+        }
+    }
+}
